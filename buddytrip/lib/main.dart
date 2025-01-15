c02e3a1ad04efca934c9e4469b991cff6ce530e4
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2025-01-15 22:46:57 +0500

    Font Family Addded, Irish Grover

--- buddytrip/lib/main.dart
+++ buddytrip/lib/main.dart
@@ -14,19 +14,24 @@ class MyApp extends StatelessWidget {
       backgroundColor: Color.fromARGB(255, 131, 127, 124),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
-            padding: EdgeInsets.only(top: 150, left: 30, right: 30, bottom: 10),
+            padding: EdgeInsets.only(top: 150, left: 50, right: 50, bottom: 10),
             child: Image.asset('assets/logo.jpg'),
           ),
           Padding(
-            padding: EdgeInsets.all(30),
+            padding: EdgeInsets.all(40),
             child: Text('The friend who knows the way ',
             style: 
             TextStyle(
               fontFamily: 'IrishGrover',
-            ),),)
-
+              fontSize: 24,
+              
+            ),
+            textAlign: TextAlign.center,
+            ),
+           ),
         ],
       ),
       ),
