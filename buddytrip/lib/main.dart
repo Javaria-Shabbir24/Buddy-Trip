8d720e103e52ba35ed0c12734c2a1f0ea72b0fb4
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2025-01-16 16:03:14 +0500

    Icon added for app launching

--- buddytrip/lib/main.dart
+++ buddytrip/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'onboardingscreen1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,30 +12,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 131, 127, 124),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    const duration = Duration(seconds: 5);
+    Timer(duration, navigateToNextPage);
+  }
+
+  void navigateToNextPage() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const Onboardingscreen1()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 131, 127, 124),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
-            padding: EdgeInsets.only(top: 150, left: 50, right: 50, bottom: 10),
+            padding: const EdgeInsets.only(
+                top: 150, left: 50, right: 50, bottom: 10),
             child: Image.asset('assets/logo.jpg'),
           ),
-          Padding(
+          const Padding(
             padding: EdgeInsets.all(40),
-            child: Text('The friend who knows the way ',
-            style: 
-            TextStyle(
-              fontFamily: 'IrishGrover',
-              fontSize: 24,
-              
+            child: Text(
+              'The friend who knows the way',
+              style: TextStyle(
+                fontFamily: 'IrishGrover',
+                fontSize: 24,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-            ),
-           ),
+          ),
+          const CircularProgressIndicator(color: Color.fromARGB(255, 49, 48, 41),),
         ],
-      ),
       ),
     );
   }
---
+++ buddytrip/lib/onboardingscreen1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Onboardingscreen1 extends StatelessWidget {
+  const Onboardingscreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      
+    );
+  }
+}
