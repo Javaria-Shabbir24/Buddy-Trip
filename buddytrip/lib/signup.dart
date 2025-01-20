b6f8c7569e560f011f0500a11619f20d5ebd87cd
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2025-01-21 01:53:55 +0500

    user authentication completed

--- buddytrip/lib/signup.dart
+++ buddytrip/lib/signup.dart
@@ -1,5 +1,7 @@
 import 'package:buddytrip/login.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -12,24 +14,26 @@ class _SignupState extends State<Signup> {
   final formKey= GlobalKey<FormState>();// key to maintain the state of the form
   final usernameController= TextEditingController();
   final passwordController= TextEditingController();
+  final emailController=TextEditingController();
   final confirmPasswordController=TextEditingController();
   @override
   void dispose(){
     usernameController.dispose();
     passwordController.dispose();
     confirmPasswordController.dispose();
+    emailController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
       ),
-      endDrawer: Drawer(),
-      body: Container(
-        color: Colors.white,
+      body: SingleChildScrollView(
+        
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -37,26 +41,25 @@ class _SignupState extends State<Signup> {
               color: Colors.black,
               thickness: 2,
             ),
-            SizedBox(height: 20,),
+            SizedBox(height: 18,),
             Center(
               child: Text('Create an Account',
               style: TextStyle(
-              fontSize: 28,
+              fontSize: 26,
               fontFamily: 'IrishGrover',
               color: Color(0xFF3C3B3E),
             ),),
             ),
-            
-            Padding(padding: EdgeInsets.all(20),
+            Padding(padding: EdgeInsets.all(25),
             child: Form(
               key: formKey, //maintaining a key for form
               child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 30,),
+                SizedBox(height: 10,),
                 Text('Username:',
                 style: TextStyle(
-                  fontSize: 22,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),),
                 SizedBox(height: 10,),
@@ -73,9 +76,31 @@ class _SignupState extends State<Signup> {
                   return null;
                 },),
                 SizedBox(height: 20,),
+                Text('Email:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),),
+                SizedBox(height: 10,),
+                TextFormField(
+                  controller: emailController,
+                decoration: InputDecoration(
+                hintText: 'Enter your email address',
+                border: OutlineInputBorder(),
+                ),
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return 'email not entered';
+                  }
+                  else if(!value.contains('@')){
+                    return 'incorrect email';
+                  }
+                  return null;
+                },),
+                SizedBox(height: 20,),
                 Text('Password:',
                 style: TextStyle(
-                  fontSize: 22,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),),
                 SizedBox(height: 10,),
@@ -99,7 +124,7 @@ class _SignupState extends State<Signup> {
             SizedBox(height: 20,),
                 Text('Confirm Password:',
                 style: TextStyle(
-                  fontSize: 22,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),),
                 SizedBox(height: 10,),
@@ -125,14 +150,26 @@ class _SignupState extends State<Signup> {
             SizedBox(height: 30,),
             Center(
               child: ElevatedButton(
-              onPressed: (){
+              onPressed: () async{
                 if(formKey.currentState!.validate()){// if all the fields are not empty
+                String name=usernameController.text.trim();
+                String password=passwordController.text.trim();
+                String email=emailController.text.trim();
+                await saveTheDataToDatabase(name,password,email);
+                //empty fields
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('User signed up successfully!'))
+                  );
+                usernameController.clear();
+                passwordController.clear();
+                confirmPasswordController.clear();
+                emailController.clear();
                 Navigator.push(context, MaterialPageRoute(builder: (context)=>Login()));
                 }
               },
             child: Text('Sign up'), ),
             ),
-            SizedBox(height: 30,),
+            SizedBox(height: 10,),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -153,4 +190,27 @@ class _SignupState extends State<Signup> {
       
     );
   }
+}
+
+//save the data to database
+Future<void> saveTheDataToDatabase(String username, String password, String email)async{
+  try{
+    CollectionReference users = FirebaseFirestore.instance.collection('Users');
+    await users.add(
+      {
+      'username':username,
+      'password':password,
+      'email':email,
+      }
+    );
+    
+    print('User Signed Up successfully');
+  }
+  on FirebaseException catch (e) {
+    print('FirebaseException: ${e.message}');
+  }
+  catch(e){
+    print('failed to add user : $e');
+  }
+
 }
