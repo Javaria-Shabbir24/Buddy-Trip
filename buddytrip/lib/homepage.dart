267c1835ad16d2d32ddd7781b1ce1ab78c38a910
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2025-01-27 00:36:54 +0500

    bottom navigation bar added

--- buddytrip/lib/homepage.dart
+++ buddytrip/lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:buddytrip/bottomnavigationbar.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
@@ -5,6 +6,25 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      endDrawer: Drawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Divider(
+              color: Colors.black,
+              thickness: 2,
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Bottomnavigationbar(),
+
+    );
   }
 }
