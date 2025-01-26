eeb25c0ffb885423fafaf33cdb5bc9910467f7b2
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2025-01-26 23:04:25 +0500

    email text added

--- buddytrip/lib/resetpassword.dart
+++ buddytrip/lib/resetpassword.dart
@@ -5,6 +5,46 @@ class Resetpassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(backgroundColor: Colors.white,),
+      body: SingleChildScrollView(
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Divider(
+            color: Colors.black,
+            thickness: 2,
+          ),
+          SizedBox(height: 20,),
+          Center(
+            child: Text('Reset Account Password',
+          style: TextStyle(
+            fontFamily: 'IrishGrover',
+            fontSize: 28,
+            color: Color(0xFF3C3B3E),
+          ),),
+          ),
+          SizedBox(height: 20,),
+          Padding(padding: EdgeInsets.all(20),
+          child: Form(
+            //set key
+            child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Email:',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+
+            ],
+          ),
+          ),
+          )
+        ],
+        ),
+      ),
+    );
   }
 }
