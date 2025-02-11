083b384b107a13768039d7c80a444b3da3d6a796
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2025-02-12 02:27:29 +0500

    image picker dependency added

--- buddytrip/lib/profile.dart
+++ buddytrip/lib/profile.dart
@@ -5,6 +5,31 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 80,),
+            Center(
+              child: Stack(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/pfp.jpg'),
+                    radius: 50,
+                  ),
+                  Positioned(
+                    top: 80,
+                    left: 80,
+                    child: Icon(Icons.edit, color: Colors.black,))
+                ],
+              ),
+
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
