6494520541042c00e823e5569007ea1c0c9f6b8d
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2025-02-12 05:02:58 +0500

    lottie dependency added for animation

--- buddytrip/lib/help.dart
+++ buddytrip/lib/help.dart
@@ -7,10 +7,27 @@ class Help extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(backgroundColor: Colors.white,),
       body: SingleChildScrollView(
-        
-      ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Divider(color: Colors.black,
+            thickness: 1,),
+            SizedBox(height: 10,),
+            Padding(padding: EdgeInsets.all(12),child:
+            Text('How can we help?',style: TextStyle(
+              fontFamily: 'IrishGrover',
+              fontSize: 24,
+              color: Color(0xFF8F8989)
+            ),
 
+            ),),
+            SizedBox(height: 10,),
+          ],
+          
+        ),
+      ),
     );
   }
 }
