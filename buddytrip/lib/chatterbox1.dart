44b9e092725346ab717b82b4706556263bf1a7b3
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2025-02-15 23:12:34 +0500

    api loaded

--- buddytrip/lib/chatterbox1.dart
+++ buddytrip/lib/chatterbox1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Chatterbox1 extends StatefulWidget {
   const Chatterbox1({super.key});
@@ -8,11 +9,34 @@ class Chatterbox1 extends StatefulWidget {
 }
 
 class _Chatterbox1State extends State<Chatterbox1> {
+  final textcontroller=TextEditingController();
+  bool isLoading=false;
+  List<Map<String,String>> messages=[];
+  String apiKey='';
+
+  //load api key
+  @override
+  void initState() {
+    loadApi();
+    super.initState();
+  }
+  Future<void> loadApi()async{
+    await dotenv.load();
+    apiKey=dotenv.env['GEMINI_API_KEY']?? "";
+  }
+
+  //send message to gemini api and get a response 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+
+          ),
+        ),
         
       ),
 
